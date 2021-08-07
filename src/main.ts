7f686538ecbfd13516408e901e857201a69562eb
georgiamoreira <georgiamoreira13@gmail.com>
2021-08-07 13:18:20 -0300

    like component

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { LikeComponent } from '../src/app/like.component';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -10,3 +11,7 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  let component = new LikeComponent(10, true);
+  component.onClick();
+  console.log(`likesCount: ${component.likesCount}, isSlected: ${component.isSelected}`);
